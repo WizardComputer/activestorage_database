cb3d642c668e8f170d59de6d5dadfc782c23b9de
Dino Maric <dino.onex@gmail.com>
2022-02-17 18:31:26 +0100

    Add support for Rails 7.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,10 +9,20 @@ ActiveRecord::Migrator.migrations_paths << File.expand_path('../db/migrate', __d
 require "rails/test_help"
 
 
-# Load fixtures from the engine
+#Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
+end
+
+class ActiveSupport::TestCase
+  setup do
+    ActiveStorage::Current.url_options = { protocol: "https://", host: "example.com", port: nil }
+  end
+
+  teardown do
+    ActiveStorage::Current.reset
+  end
 end
